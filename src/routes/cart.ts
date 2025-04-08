2f636f77bb9737ed60155f1a9bce0ef80cafcf27
Krunal M Pokar <129573891+krunal-99@users.noreply.github.com>
2025-04-08 14:52:50 +0530

    Adding clear and update cart routes.

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import {
+  clearCart,
   getAllCartItems,
   getCartByUser,
   removeFromCart,
+  updateCartItem,
 } from "../controllers/cart";
 
 const router = express.Router();
@@ -11,6 +13,10 @@ router.get("/", getAllCartItems);
 
 router.get("/:userId", getCartByUser);
 
+router.patch("/:itemId", updateCartItem);
+
 router.delete("/:itemId", removeFromCart);
+
+router.delete("/clear/:userId", clearCart);
 
 export default router;
