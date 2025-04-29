0863b3091b6bcad7e1bf8b7e4dc5f4fe9c661cfe
Krunal M Pokar <129573891+krunal-99@users.noreply.github.com>
2025-04-29 17:01:33 +0530

    Removed the password validation check during login.

--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -14,17 +14,6 @@ class LoginUser {
   email: string;
 
   @IsString()
-  @MinLength(8, { message: "Password must be at least 8 characters long." })
-  @Matches(/(?=.*[a-z])/, {
-    message: "Password must contain at least one lowercase letter",
-  })
-  @Matches(/(?=.*[A-Z])/, {
-    message: "Password must contain at least one uppercase letter",
-  })
-  @Matches(/(?=.*\d)/, { message: "Password must contain at least one number" })
-  @Matches(/(?=.*[\W_])/, {
-    message: "Password must contain at least one special character",
-  })
   password: string;
 
   constructor(email: string, password: string) {
