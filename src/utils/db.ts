bf7eb1e614bd5b4a6f0f5669360b51c3f661dfa4
Krunal M Pokar <129573891+krunal-99@users.noreply.github.com>
2025-05-02 15:53:48 +0530

    Solving the render deployment errors.

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,7 @@
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, NODE_ENV } = process.env;
-
-const isProduction = NODE_ENV === "production";
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -12,10 +10,10 @@ export const AppDataSource = new DataSource({
   username: PGUSER,
   password: PGPASSWORD,
   port: 5432,
-  entities: isProduction ? ["./dist/entities/*.js"] : ["./src/entities/*.ts"],
-  synchronize: !isProduction,
+  entities: ["./dist/entities/*.js"],
+  // entities: ["./src/entities/*.ts"],
+  synchronize: true,
   ssl: { rejectUnauthorized: false },
-  migrations: isProduction
-    ? ["./dist/migrations/*.js"]
-    : ["./src/migrations/*.ts"],
+  // migrations: ["./src/migrations/*.ts"],
+  migrations: ["./dist/migrations/*.js"],
 });
